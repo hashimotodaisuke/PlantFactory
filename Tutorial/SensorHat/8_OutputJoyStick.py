8dfb24e5eb5746a23237fd9480d93bffca073f6b
hashimotodaisuke <hashi.uniden@gmail.com>
2021-05-28 08:55:15 +0900

    add buffer index to modify error

--- Tutorial/SensorHat/8_OutputJoyStick.py
+++ Tutorial/SensorHat/8_OutputJoyStick.py
@@ -3,8 +3,8 @@ from sense_hat import SenseHat # refer https://pythonhosted.org/sense-hat/api/#j
 
 sh=SenseHat()
 while True:
-    event = sh.stick.get_events():
+    event = sh.stick.get_events()
     if event:
-        # print( event.action, event.direction)
-        print( '%s-%s' %(event.action, event.direction))
+        # print( event[0].action, event[0].direction)
+        print( '%s-%s' %(event[0].action, event[0].direction))
     sleep(0.3)
