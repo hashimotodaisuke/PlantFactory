dacca23939e957f6d120d244a1aaa745a32bd0d9
hashimotodaisuke <hashi.uniden@gmail.com>
2021-06-07 12:37:43 +0900

    modify Tutorial.txt and add Coral Turorail

--- Tutorial/Coral/EdgeTpuRuntime_install.sh
+++ Tutorial/Coral/EdgeTpuRuntime_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install libedgetpu1-max
+sudo apt update
+sudo apt install libedgetpu1-max
+sudo apt install python3-pycoral
