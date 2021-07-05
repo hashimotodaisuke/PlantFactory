cb1dfb99929a44d97c251fa3682890e815372b3d
hashimotodaisuke <hashi.uniden@gmail.com>
2021-07-05 09:19:13 +0900

    modify SQL and add RealTimeGraphHomeWork in Flask

---
+++ Tutorial/Flask/7_RealTimeGraphHomeWork/7_RealTimeGraph.py
@@ -0,0 +1,33 @@
+import os
+import json
+import datetime
+import random
+import time
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+from flask import Flask, request, render_template
+from sense_emu import SenseHat
+
+app = Flask(__name__)
+sh = SenseHat()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/publish')
+def publish():
+    if request.environ.get('wsgi.websocket'):
+        ws = request.environ['wsgi.websocket']
+        while True:
+            t = int(time.mktime(datetime.datetime.now().timetuple()))
+            ws.send(json.dumps([{"time": t, "y": sh.temperature},
+                                {"time": t, "y": sh.humidity},
+                                {"time": t, "y": sh.pressure}]))
+            time.sleep(1)
+    return
+
+if __name__ == '__main__':
+    app.debug = True
+    server = pywsgi.WSGIServer(('localhost', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
